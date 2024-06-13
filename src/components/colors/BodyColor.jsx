332abae251de8c95fb2167c30feb3e80749bb920
Milad Naeini <milad88884@gmail.com>
2024-06-13 16:49:46 +0200

    feat: added pain feature to the sl63

--- src/components/colors/BodyColor.jsx
+++ src/components/colors/BodyColor.jsx
@@ -2,23 +2,30 @@ import { useEffect, useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 
-export const BodyColor = (group, bodyColor) => {
+export const BodyColor = (group, bodyColor, nextCar) => {
   const currentColor = useRef(new THREE.Color(bodyColor));
   const targetColor = useRef(new THREE.Color(bodyColor));
   const startTime = useRef(null);
+  if (nextCar === "models/car/MB-w2222/scene.gltf") {
+    var x = 12;
+    var y = 11;
+  }
+  if (nextCar === "models/car/MB-SL63/scene.gltf") {
+    var x = 9;
+    var y = 4;
+  }
 
   useEffect(() => {
-    if (group && group.children[12] && group.children[12].children[11]) {
+    if (group && group.children[x] && group.children[x].children[y]) {
       targetColor.current.set(bodyColor);
       startTime.current = performance.now();
     }
   }, [group, bodyColor]);
 
   useFrame(() => {
-    if (!group || !group.children[12] || !group.children[12].children[11])
-      return;
+    if (!group || !group.children[x] || !group.children[x].children[y]) return;
 
-    const material = group.children[12].children[11].material;
+    const material = group.children[x].children[y].material;
     if (!material) return;
 
     const elapsedTime = (performance.now() - startTime.current) / 1000;
